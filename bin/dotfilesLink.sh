7558b94dc3909c0722a26142ae256c256a4fbbfa
raru-ex <raru@syrup16g.net>
2018-08-16 02:21:27 +0900

    change file structure

---
+++ bin/dotfilesLink.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.vim ~/.vim
+# ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.ctags ~/.ctags
+# ln -sf ~/dotfiles/.config ~/.config
---
+++ bin/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for f in .??*
+do
+    [[ "$f" == ".git" ]] && continue
+    [[ "$f" == ".DS_Store" ]] && continue
+    [[ "$f" == ".gitignore" ]] && continue
+
+    echo "$f"
+done
