21a92a91fd8b1d7daa61ca2d7e08cf54ddeea5f0
藪崎卓巳 <raru@syrup16g.net>
2018-08-17 01:47:36 +0900

    add readme

--- bin/dotfilesLink2nvim.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-ln -sf ~/dotfiles/init.vim ~/.config/nvim/init.vim
-ln -sf ~/dotfiles/vimrcs ~/.config/nvim/vimrcs
-ln -sf ~/dotfiles/dict ~/.config/nvim/vimrcs
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
