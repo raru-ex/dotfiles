c35e66baaa950415f12294cde9471d56614cb7fd
藪崎卓巳 <raru@syrup16g.net>
2018-11-05 00:43:37 +0900

    add zsh settings

--- bin/install.sh
+++ bin/install.sh
@@ -4,3 +4,4 @@ ln -sf ~/dotfiles/.vim ~/.vim
 # ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.ctags ~/.ctags
 # ln -sf ~/dotfiles/.config ~/.config
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
