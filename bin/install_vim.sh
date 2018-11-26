4a03549fbdbaab012df1964dec17aa5af4dee13b
藪崎卓巳 <raru@syrup16g.net>
2018-11-26 22:28:15 +0900

    change vim install shell

--- bin/install_vim.sh
+++ bin/install_vim.sh
@@ -10,14 +10,25 @@ ln -nfs ~/dotfiles/.vimrc ~/.config/nvim/init.vim
 ln -nfs ~/dotfiles/.ctags ~/.ctags
 
 # set up ensime
+echo "[INFO] set up ensime"
 if [ -e ~/.sbt/1.0/plugins ]; then
   mkdir -p ~/.sbt/1.0/plugins/
 fi
 
+echo "[INFO] install pip libs for ensime"
+if type pip2 > /dev/null 2>&1; then
+  pip2 install sexpdata websocket-client
+fi
+if type pip3 > /dev/null 2>&1; then
+  pip3 install sexpdata websocket-client
+fi
+
+echo "[INFO] add sbt settings"
 grep sbt-ensime ~/.sbt/1.0/plugins/plugins.sbt > /dev/null
 if [ $? -eq 1 ]; then
   echo 'addSbtPlugin("org.ensime" % "sbt-ensime" % "2.5.1")' >> ~/.sbt/1.0/plugins/plugins.sbt
 fi
+
 grep ensime ~/.sbt/1.0/global.sbt > /dev/null
 if [ $? -eq 1 ]; then
   echo 'ensimeIgnoreMissingDirectories := true' >> ~/.sbt/1.0/global.sbt
@@ -26,3 +37,4 @@ else
   echo 'ensimeIgnoreMissingDirectories := true' > ~/.sbt/1.0/global.sbt
   echo 'ensimeIgnoreSourcesInBase := true' >> ~/.sbt/1.0/global.sbt
 fi
+echo "[INFO] finished"
