cde2cc1558c1f74192c6531328917bbe388f62a9
raru-ex <raru@syrup16g.net>
2019-06-08 19:57:51 +0900

    change tmux settings

--- bin/install_tmux.sh
+++ bin/install_tmux.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
 if type brew > /dev/null 2>&1; then
-  echo "install tmux"
+  echo "[INFO] install tmux"
   brew install tmux
-  echo "finished"
-  echo "create tmux symlink"
+  echo "[INFO] install tmux plugin manager"
+  mkdir -p ~/.tmux/plugins/
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  echo "[INFO] create tmux symlink"
   # tmux
   ln -nfs ~/dotfiles/.tmux.conf ~/.tmux.conf
-  echo "finish"
+  echo "[INFO] finished install tmux"
 else
-  echo "brew is not installed. please install homebrew"
+  echo "[ERROR] brew is not installed. please install homebrew"
 fi
 
 # tmux用のバッテリー表示スクリプトの権限修正
