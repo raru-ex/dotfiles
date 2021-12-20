20cb7d174a8635191464b787fde054b99f89ecf6
raru-ex <raru@syrup16g.net>
2021-12-20 15:14:52 +0900

    change font install shell for wls2 ubuntu

--- bin/install_fonts.sh
+++ bin/install_fonts.sh
@@ -4,12 +4,20 @@
 emulate -R sh
 
 if type brew > /dev/null 2>&1; then
-  echo "[INFO] install Ricty"
-  brew tap sanemat/font
-  brew install ricty
-  cp -f /usr/local/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts/
-  fc-cache -vf
-  echo "[INFO] Ricty font install finished"
+
+  if [ "$(uname)" == 'Darwin' ]; then
+    echo "[INFO] install Ricty"
+    brew tap sanemat/font
+    brew install ricty
+    cp -f /usr/local/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts/
+    fc-cache -vf
+    echo "[INFO] Ricty font install finished"
+  elif [ -n "$(which wslpath)" ]; then
+    echo "[INFO] Please Install hakugen or other fonts"
+  elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+    echo "[INFO] Please Install any font"
+  fi
+
 else
   echo "[WARN] brew is not installed. Please install homebrew"
 fi
