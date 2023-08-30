792a442f4eb8d198ca3d3823fee9cf2a4351583d
raru-ex <raru@syrup16g.net>
2023-08-30 22:53:52 +0900

    change: nvimのplugin managerをpackerからlazyに変更

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,113 +1,114 @@
 local config = require('libs.config_loader')
 local scheme = require('libs.color_scheme')
 
-vim.cmd('packadd packer.nvim')
-vim.cmd([[
-augroup packer_user_config
-autocmd!
-autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-augroup end
-]])
+-- Initialize package manager: Lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-return require('packer').startup(function(use)
-  use { 'wbthomason/packer.nvim', opt = true }
-
-  -- syntax
-  use {
+require("lazy").setup({
+  {
     'nvim-treesitter/nvim-treesitter',
     commit = "3af1220e18034eb2ce7d1c8e77055bc3bf3c1c36",
-    run = function()
+    build = function()
       local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
       ts_update()
     end,
     config = config.load 'nvim-treesitter'
-  }
-
+  },
+ 
   -- terminal
-  use { 'akinsho/toggleterm.nvim', tag = '*', config = config.load 'toggleterm' }
+  { 'akinsho/toggleterm.nvim', version = '*', config = config.load 'toggleterm' },
 
   -- git
-  use { 'sindrets/diffview.nvim' }
+  { 'sindrets/diffview.nvim' },
 
   -- filer
-  use {
+  {
   "nvim-neo-tree/neo-tree.nvim",
     branch = "v2.x",
-    requires = { 
+    dependencies = { 
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim",
     },
     config = config.load 'neotree',
-  }
+  },
 
   -- status line
-  use {
+  {
     'nvim-lualine/lualine.nvim',
-    requires = {
-      { 'kyazdani42/nvim-web-devicons', opt = true },
+    dependencies = {
+      { 'kyazdani42/nvim-web-devicons', opt = true, name = 'status-nvim-web-devicons'},
       { 'arkav/lualine-lsp-progress' },
       { 'hrsh7th/vim-vsnip' }
     },
     config = config.load 'lualine',
-    wants = { 'vim-vsnip' },
-  }
+  },
 
   -- color scheme
-  -- use { 'folke/tokyonight.nvim', config = scheme.setup 'tokyonight' }
-  use { 'doums/darcula', config = scheme.setup 'darcula' }
+  -- { 'folke/tokyonight.nvim', config = scheme.setup 'tokyonight' }
+  { 'doums/darcula', config = scheme.setup 'darcula' },
 
   -- indent, white space
-  use { 'lukas-reineke/indent-blankline.nvim', config = config.load 'indent-blankline' }
-  use { 'echasnovski/mini.indentscope', branch = 'stable' , config = config.load 'mini-indentscope'}
+  { 'lukas-reineke/indent-blankline.nvim', config = config.load 'indent-blankline' },
+  { 'echasnovski/mini.indentscope', branch = 'stable' , config = config.load 'mini-indentscope'},
 
   -- utils
-  use { 'FooSoft/vim-argwrap', config = config.load 'arg-wrap'}
+  { 'FooSoft/vim-argwrap', config = config.load 'arg-wrap'},
 
   -- LSP/Completion
-  use { 'github/copilot.vim', config = config.load 'copilot' }
-  use {
-    'neoclide/coc.nvim', branch='release', config = config.load 'coc'
-  }
+  { 'github/copilot.vim', config = config.load 'copilot' },
+  { 'neoclide/coc.nvim', branch='release', config = config.load 'coc' },
 
   -- fizzy finder
-  use {
+  {
     'nvim-telescope/telescope.nvim', tag = '0.1.1',
     -- or                            , branch = '0.1.x',
-    requires = {
+    dependencies = {
       {'nvim-lua/plenary.nvim'},
       -- { '~/.fzf', rtp = 'fzf'}
       -- {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
       { 'fannheyward/telescope-coc.nvim' },
     },
     config = config.load 'telescope',
-  }
-  use {
+  },
+  {
     'nvim-telescope/telescope-frecency.nvim',
     config = function()
       require'telescope'.load_extension('frecency')
     end,
-    requires = {'kkharji/sqlite.lua'}
-  }
+    dependencies = {'kkharji/sqlite.lua'},
+  },
 
-  -- lazy
-  use { 'plasticboy/vim-markdown', config = config.load 'markdown', ft = { 'md', 'mkd', 'markdown' } }
-  use { 'mattn/vim-goimports', config = config.load 'go-imports', ft = { 'go' } }
-  use { '110y/vim-go-expr-completion', config = config.load 'go-expr-completion', ft = { 'go' } }
+
+  -- for any filetype
+  { 'plasticboy/vim-markdown', config = config.load 'markdown', ft = { 'md', 'mkd', 'markdown' } },
+  { 'mattn/vim-goimports', config = config.load 'go-imports', ft = { 'go' } },
+  { '110y/vim-go-expr-completion', config = config.load 'go-expr-completion', ft = { 'go' } },
+
 
   -- test
-  use {
+  {
     "nvim-neotest/neotest",
-    requires = {
+    dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
       "antoinemadec/FixCursorHold.nvim",
       "nvim-neotest/neotest-go"
     },
     config = config.load 'neotest',
-  }
+  },
+  { 'buoto/gotests-vim', config = config.load 'gotests-vim', ft = { 'go' } },
 
-  use { 'buoto/gotests-vim', config = config.load 'gotests-vim', ft = { 'go' } }
-end
-)
+})
 
