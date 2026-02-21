f28b37eeafbada787c79a04e028a187f46a12049
raru-ex <raru@syrup16g.net>
2026-02-21 23:44:23 +0900

    change

--- bin/install_tmux.sh
+++ bin/install_tmux.sh
@@ -5,7 +5,11 @@ if type brew > /dev/null 2>&1; then
   brew install tmux
   echo "[INFO] install tmux plugin manager"
   mkdir -p ~/.tmux/plugins/
-  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  if [ -d ~/.tmux/plugins/tpm ]; then
+    echo "[INFO] tpm already installed, skipping"
+  else
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  fi
   echo "[INFO] create tmux symlink"
   ln -nfs ~/dotfiles/.tmux.conf ~/.tmux.conf
   ln -nfs ~/dotfiles/.tmux/bin ~/.tmux/
@@ -16,3 +20,4 @@ fi
 
 # tmux用のバッテリー表示スクリプトの権限修正
 chmod 755 ~/.tmux/bin/battery
+chmod 755 ~/.tmux/bin/tmuxpopup
